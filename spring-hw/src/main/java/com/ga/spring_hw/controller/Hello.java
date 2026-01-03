57eecdb2e7950b3ed760b6fdce7267896f42bbf5
Zahralsayed <zalmatooq31@gmail.com>
2026-01-03 10:46:20 +0300

    Step 1 - COMPLETED

--- spring-hw/src/main/java/com/ga/spring_hw/controller/Hello.java
+++ spring-hw/src/main/java/com/ga/spring_hw/controller/Hello.java
@@ -1,4 +1,15 @@
 package com.ga.spring_hw.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class Hello {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
 }
